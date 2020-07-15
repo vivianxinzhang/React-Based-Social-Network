cb68f5ec99028181c70145f3da12d8dfe49a3e94
Vivian Zhang <vivianxinzhang@gmail.com>
2020-07-15 15:31:41 -0700

    update register

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/App.css';
 import TopBar from "./TopBar";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Register from './Register';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+                <Register/>
+            </div>
+        );
+    }
+}
+
+export default Main;
