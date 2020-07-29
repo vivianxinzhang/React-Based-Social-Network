a207df091472162a1068757383ea5860a8fc778f
wx6698 <wx6698@WangXins-MacBook-Air.local>
2020-07-28 19:35:37 -0700

    add gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Gallery extends Component {
+    render() {
+        return (
+            <div>
+                Gallery
+            </div>
+        );
+    }
+}
+
+export default Gallery;
