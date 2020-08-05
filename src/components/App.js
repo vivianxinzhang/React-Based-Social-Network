de03b6cb8469533898bd9b38f55f91ec5d4a575a
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-05 11:03:10 -0700

    handle login

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import '../styles/App.css';
 import TopBar from "./TopBar";
 import Main from "./Main";
+import { TOKEN_KEY} from "../constants";
 
-function App() {
-  return (
-    <div className="App">
-      <TopBar/>
-      <Main/>
-    </div>
-  );
+class App extends React.Component{
+    state = {
+        isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY))
+    }
+
+    handleLoginSucceed = (token) => {
+        console.log(token);
+        localStorage.setItem(TOKEN_KEY, token);
+        this.setState({
+            isLoggedIn: true
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TopBar/>
+                <Main isLoggedIn = {this.state.isLoggedIn}
+                      handleLoginSucceed = {this.handleLoginSucceed}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,21 +3,32 @@ import Register from './Register';
 import Login from "./Login";
 import Home from "./Home";
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect} from "react-router-dom";
 
 class Main extends Component {
     render() {
         return (
             <div className="main">
-                {/*<Register/>*/}
-                {/*<Login />*/}
                 <Switch>
                     <Route path="/register" component={Register}/>
-                    <Route path="/login" component={Login}/>
+                    <Route path="/login"
+                           // component={Login} need to know if logged in already
+                           render = {this.getLogin}
+                    />
                     <Route path="/home" component={Home}/>
                 </Switch>
             </div>
         );
+    }
+    getLogin = () => {
+        // case 1: if already login -> <Home />
+
+        // case 2: not yet -> <Login />
+        return this.props.isLoggedIn ?
+            <Redirect to="/home"/> :
+                <Login isLoggedIn = {this.props.isLoggedIn}
+                handleLoginSucceed = {this.props.handleLoginSucceed} />
+
     }
 }
 
