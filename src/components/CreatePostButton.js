0b54d3643106d164ad7adfa804606c1078faa1b4
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-08 17:56:10 -0700

    click to open modal

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import { Modal, Button } from 'antd';
 
 class CreatePostButton extends Component {
+    state = { visible: false};
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
     render() {
+        // 解构
+        const { visible } = this.state;
         return (
             <div>
-                Create New Post
+                <Button type = "primary" onClick={this.showModal}>
+                    Create New Post
+                </Button>
+                <Modal title="Create New Post" visible={visible}>
+                    Hello
+                </Modal>
             </div>
         );
     }
