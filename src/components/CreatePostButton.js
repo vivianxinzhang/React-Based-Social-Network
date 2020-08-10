5e04d7b3825801407753516845a0b2948bd1a5d8
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-09 20:27:24 -0700

    modal showup setting

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -2,10 +2,25 @@ import React, {Component} from 'react';
 import { Modal, Button } from 'antd';
 
 class CreatePostButton extends Component {
+    // 通过 state 来决定 modal 是否可见
     state = { visible: false};
     showModal = () => {
         this.setState({
             visible: true,
+        });
+    };
+
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
         });
     };
 
@@ -17,7 +32,11 @@ class CreatePostButton extends Component {
                 <Button type = "primary" onClick={this.showModal}>
                     Create New Post
                 </Button>
-                <Modal title="Create New Post" visible={visible}>
+                <Modal title="Create New Post"
+                       visible={visible}
+                       onOk = {this.handleOk}
+                       onCancel={this.handleCancel}
+                >
                     Hello
                 </Modal>
             </div>
