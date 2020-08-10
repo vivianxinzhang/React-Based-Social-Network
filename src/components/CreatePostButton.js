8716844e73a522025eaf9418bf21ebc2e10f70e4
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-09 22:36:35 -0700

    add upload in form

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 import { Modal, Button } from 'antd';
+import CreatePostForm from "./CreatePostForm";
 
 class CreatePostButton extends Component {
     // 通过 state 来决定 modal 是否可见
-    state = { visible: false};
+    state = {
+        visible: false,
+        confirmLoading: false,
+    };
     showModal = () => {
         this.setState({
             visible: true,
@@ -26,7 +30,7 @@ class CreatePostButton extends Component {
 
     render() {
         // 解构
-        const { visible } = this.state;
+        const { visible, confirmLoading } = this.state;
         return (
             <div>
                 <Button type = "primary" onClick={this.showModal}>
@@ -36,11 +40,16 @@ class CreatePostButton extends Component {
                        visible={visible}
                        onOk = {this.handleOk}
                        onCancel={this.handleCancel}
+                       confirmLoading={confirmLoading}
+                       okText="Create"
                 >
-                    Hello
+                    <CreatePostForm ref={this.getRefForm}/>
                 </Modal>
             </div>
         );
+    }
+    getRefForm = () => {
+
     }
 }
 
---
+++ src/components/CreatePostForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CreatePostForm extends Component {
+    render() {
+        return (
+            <div>
+               create form
+            </div>
+        );
+    }
+}
+
+export default CreatePostForm;
