29aea98311ea4a3372e88f0f18e0ec5e7a4c741c
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-09 23:37:03 -0700

    create post form UI

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -59,6 +59,7 @@ class CreatePostButton extends Component {
             </div>
         );
     }
+
     getRefForm = (formObj) => {
         console.log(formObj);
     }
--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
+import { Form, Upload, Input, Icon } from "antd";
 
-class CreatePostForm extends Component {
+class NormalCreatePostForm extends Component {
     render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
+
         return (
-            <div>
-               create form
-            </div>
+            <Form {...formItemLayout}>
+                <Form.Item label="Message">
+                    {
+                        getFieldDecorator('message', {
+                            rules: [{
+                                required: true,
+                                message: 'Please input your message'
+                            }]
+                        })(<Input />)
+                    }
+                </Form.Item>
+                <Form.Item label="Image/Video">
+                    {
+                        getFieldDecorator('image', {
+                            rules: [{
+                                required: true,
+                                message: 'Please upload your image'
+                            }]
+                        })(
+                            <Upload.Dragger>
+
+                            </Upload.Dragger>
+                            )
+                    }
+                </Form.Item>
+            </Form>
         );
     }
 }
-
+// HOC to get form object
+const CreatePostForm = Form.create()(NormalCreatePostForm);
 export default CreatePostForm;
