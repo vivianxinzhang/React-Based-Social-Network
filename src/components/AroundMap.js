dce5871cbe08a779d5f6e52ec54d854e11b347a0
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-14 20:27:41 -0700

    get center and radius of map

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -8,8 +8,11 @@ class NormalAroundMap extends Component {
         const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
         return (
             <GoogleMap
+                ref={this.getMapRef}
                 defaultZoom={10}
                 defaultCenter={{ lat: lat, lng: lon }}
+                onDragEnd={this.reloadMarker}
+                onZoomChanged={this.reloadMarker}
             >
                 {
                     this.props.posts.map(
@@ -18,6 +21,43 @@ class NormalAroundMap extends Component {
                 }
             </GoogleMap>
         );
+    }
+
+    getMapRef = (mapInstance) => {
+        console.log(mapInstance);
+        this.map = mapInstance;
+    }
+
+    reloadMarker = () => {
+        console.log(1);
+        // get location
+        const center = this.getCenter();
+        // get radius
+        const radius = this.getRadius();
+        // reload post -> call this.props.loadPostsByTopic (child -> parent)
+
+    }
+
+    getCenter = () => {
+        const center = this.map.getCenter();
+        console.log('center -> ', center);
+        return {
+            lat: center.lat(),
+            lon: center.lng()
+        };
+    }
+
+    getRadius = () => {
+        const bounds = this.map.getBounds();
+        console.log('bounds -> ', bounds);
+        const center = this.map.getCenter();
+        if (center && bounds) {
+            const ne = bounds.getNorthEast();
+            console.log('ne -> ', ne);
+            const right = new window.google.maps.LatLng(center.lat(), ne.lng());
+            console.log('right -> ', right);
+            return 0.001 * window.google.maps.geometry.spherical.computeDistanceBetween(center, right);
+        }
     }
 }
 
