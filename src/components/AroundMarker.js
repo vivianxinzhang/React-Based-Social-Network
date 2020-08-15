515b6507e6da52194ac0a44c0b4a039aeaee271b
Vivian Zhang <vivianxinzhang@gmail.com>
2020-08-14 17:59:48 -0700

    add info-window for map marker

--- src/components/AroundMarker.js
+++ src/components/AroundMarker.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
-import { Marker } from "react-google-maps";
+import { Marker, InfoWindow } from "react-google-maps";
 import PropTypes from 'prop-types';
 
 class AroundMarker extends Component {
     static  propTypes = {
         post: PropTypes.object.isRequired
+    }
+
+    state = {
+        isOpen: false
+    }
+
+    // handleToggle = () => {
+    //     this.setState((prevState) => ({
+    //             isOpen: !prevState.isOpen
+    //         })
+    //     );
+    // }
+
+    handleToggle = () => {
+        this.setState((prevState) => {
+                return {
+                    isOpen: !prevState.isOpen
+                }
+            }
+        );
     }
 
     render() {
@@ -13,7 +33,20 @@ class AroundMarker extends Component {
         return (
             <Marker
                 position={{ lat: lat, lng: lon }}
-            />
+                onClick={this.handleToggle}
+            >
+                {
+                    this.state.isOpen ?
+                        (
+                            <InfoWindow>
+                                <div>
+                                    <img src={url} alt={message} className="around-marker-image"/>
+                                    <p>{`${user}: ${message}`}</p>
+                                </div>
+                            </InfoWindow>
+                        ) : null
+                }
+            </Marker>
         );
     }
 }
